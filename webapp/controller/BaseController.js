09eae14c7bd6a125d044adddb7be53ed4a79d96a
gmahi <gullamahi@gmail.com>
2017-01-03 13:27:14 +0000

    Odata Filter

---
+++ webapp/controller/BaseController.js
@@ -0,0 +1,21 @@
+// BaseController.js
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+	return Controller.extend("sapui5.demo.odata.readingdata.bestpractice.BaseController", {
+		getRouter: function() {
+			return sap.ui.core.UIComponent.getRouterFor(this);
+		},
+		getModel: function(sName) {
+			return this.getView().getModel(sName);
+		},
+		setModel: function(oModel, sName) {
+			return this.getView().setModel(oModel, sName);
+		},
+		getResourceBundle: function() {
+			return this.getOwnerComponent().getModel("i18n")
+				.getResourceBundle();
+		}
+	});
+});
